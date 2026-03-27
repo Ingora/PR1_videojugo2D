31f52aa7cc83e635785c110989a366022b8056bb
Ingora <inesgomezrastoll@gmail.com>
2026-03-27 14:27:37 +0100

    añadido salto y caminado

--- Assets/Scripts/MovMaguita.cs
+++ Assets/Scripts/MovMaguita.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class MovMaguita : MonoBehaviour
 {
+
+    int miNumero = 1;
+
+    float miNumeroFlotante = 0.8f;
+
+    string miCadenaDetexto = "Hola esto es una cadena de texto";
+
+    bool esEstrella = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        float sumaEntreDecenas = Sumar (10,20,3.8f);
+        Debug.Log("inicio");
+        Debug.Log (sumaEntreDecenas);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Hola");
     }
+
+    float Sumar(int Num1, int Num2, float Num3)
+    {
+        float suma = Num1 + Num2 + Num3;
+        return suma;
+    }
+
+
+
+
 }
